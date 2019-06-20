70a7dde935cd4fd8c9a44c1b3bfbbd2e87135e8c
zuijin <bylingbird@gmail.com>
2019-06-20 13:49:29 +0800

    关于

--- src/EdgeClient/Forms/AboutWindow.xaml.cs
+++ src/EdgeClient/Forms/AboutWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -21,6 +22,12 @@ namespace EdgeClient.Forms
         public AboutWindow()
         {
             InitializeComponent();
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            Hyperlink link = sender as Hyperlink;
+            Process.Start(new ProcessStartInfo(link.NavigateUri.AbsoluteUri));
         }
     }
 }
